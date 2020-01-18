e5a94df38f6426bf71f4a27f1983ae389d9fd535
n31 <n31@users.noreply.github.com>
2020-01-18 17:20:02 +0300

    first commit

--- Exam tickets/1/1.cpp
+++ Exam tickets/1/1.cpp
@@ -1,14 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <ctime>
+
 
 int main() {
-	int size;
-	int **matrix = (int**)malloc(size * sizeof(int*));
-	for (int i = 0; i < size; i++) matrix[i] = (int)malloc(size * sizeof(int));
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			scanf("%d", matrix[i][j]);
+	srand(time(0));
+	int n;
+	scanf("%d", &n);
+	int **arr = (int**)malloc(n * sizeof(int*));
+	for (int i = 0; i < n; i++) {
+		arr[i] = (int*)malloc(n * sizeof(int));
+		for (int j = 0; j < n; j++) {
+			arr[i][j] = rand() % 10;
+			printf("%d ", arr[i][j]);
 		}
+		printf("\n");
 	}
+
+	int i = n - 1;
+	while (i >= 0) {
+		int j = n - 1;
+		while (j >= 0) {
+				if (i == j) break;
+				int tmp = arr[i][j];
+				arr[i][j] = arr[j][i];
+				arr[j][i] = tmp;
+			j--;
+		}
+		i--;
+	}
+
+	printf("\n\n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
 }
